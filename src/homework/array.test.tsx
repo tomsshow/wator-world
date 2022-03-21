a3483c22fde4e524b8499b3e3a090c8fb0f1c7fd
Tom Varughese <shiju.tomvarughese@gmail.com>
2022-03-21 18:33:51 +0000

    3-21 homework added

---
+++ src/homework/array.test.tsx
@@ -0,0 +1,38 @@
+
+
+function getMaxInArray(array:number[]):number {
+
+}
+
+function doesArrayContainString(needle:string, haystack:string[]):boolean {
+
+}
+
+function getRandomElementInArray(array:number[]):number {
+
+}
+
+function countNumberOfNegativeNumbersInArray(array:number[]):number {
+
+}
+
+
+//
+//. countNumberOfInstancesInArray([1, 2, 3, 1, 2, 1, 1], 1) -> 4
+function countNumberOfInstancesInArray(array:number[], valueToSearchFor:number):number {
+
+}
+
+//
+// example: 
+//
+//.    function isEven(n) { return n % 2 == 0 }
+//
+//.  then
+//
+//.     expect(countNumberOfMatchingNumberInArray([1,2,3], isEven)).toBe(1)
+//
+//. since there is only one number for which isEven(x) is true
+function countNumberOfMatchingNumberInArray(array:number[], matchFn: (v:number)=>boolean):number {
+
+}
