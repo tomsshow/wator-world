103d32794cf12432ecac196d1317eb0bc704cdba
Tom Varughese <shiju.tomvarughese@gmail.com>
2022-03-21 03:35:50 +0000

    fixed two adjacent move to one spot test

--- src/state/fish.test.tsx
+++ src/state/fish.test.tsx
@@ -16,16 +16,24 @@ test('when there is an empty adjacent location, we move to it', ()=>{
 
 test('when all adjacent locations already have a fish, we stay in place', ()=> {
     let redFish = new Fish(1,1);
-    let blueFish = new Fish(1,2);
+    let blueFish1 = new Fish(1,0);
+    let blueFish2 = new Fish(0,1);
+    let blueFish3 = new Fish(2,1);
+    let blueFish4 = new Fish(1,2);
+
+
 
     let world = new World(3,3);
     world.addEntity(redFish);
-    world.addEntity(blueFish);
-    
+    world.addEntity(blueFish1);
+    world.addEntity(blueFish2);
+    world.addEntity(blueFish3);
+    world.addEntity(blueFish4);
+
     // replace the implementation of world.getAdjacentLocations with a mock 
     // that only returns one square: the square containing bluefish.
     // this replaces the random locaiton with a location we can test the scenario with
-    jest.spyOn(world, "getAdjacentLocations").mockImplementation((x,y) => new Set([[blueFish.x, blueFish.y]]));
+    jest.spyOn(world, "getAdjacentLocations").mockImplementation((x,y) => new Set([[redFish.x, redFish.y]]));
 
     redFish.tick(world);
 
@@ -34,29 +42,33 @@ test('when all adjacent locations already have a fish, we stay in place', ()=> {
 
     //visual expectation    
 //   0    1    2   
-// [   ][   ][   ] 0
-// [   ][ r ][   ] 1
+// [   ][ b ][   ] 0
+// [ b ][ r ][ b ] 1
 // [   ][ b ][   ] 2
 })
 
 test('when there are two adjacent locations and one is full, we move to the empty location', () => {
     // TODO implement me
-    let redFish = new Fish(1,1);
-    let blueFish = new Fish(1,2);
+    let redFish = new Fish(0,1);
+    let blueFish = new Fish(0,0);
     
+
     let world = new World(3,3);
     world.addEntity(redFish);
     world.addEntity(blueFish);
 
-    jest.spyOn(world, "getAdjacentLocations").mockImplementation((x,y) => new Set([[blueFish.x, blueFish.y], [1,0]]));
+    jest.spyOn(world, "getAdjacentLocations").mockImplementation((x,y) => new Set([[1, 1]])); // need nudge on making this more dynamic
 
+    redFish.tick(world);
 
-    expect(false).toEqual(true);
-    //visual expectation    
-//   0    1    2   
-// [   ][   ][   ] 0
-// [   ][ r ][   ] 1
-// [   ][ b ][   ] 2
+    expect(redFish.x).toEqual(1);
+    expect(redFish.y).toEqual(1);
+    
+
+//visual expectation    
+//   0    1     
+// [ b ][   ] 0
+// [ r ][ e ] 1
 })
 
 test('when a fish is not ready to breed and there is an adjacent empty location, we move there and our old location is empty', () => {
